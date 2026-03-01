f0fae3c343482678a6189939f4f3b65e1402fd70
l2welch <livia.welch3@gmail.com>
2026-02-28 21:12:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const divider = document.querySelector('.divider');
+const leftPanel = document.querySelector('.left');
 
+let isDragging = false;
+
+divider.addEventListener('mousedown', () => {
+  isDragging = true;
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDragging) return;
+
+  const containerWidth = window.innerWidth;
+  const newLeftWidth = (e.clientX / containerWidth) * 100;
+
+  leftPanel.style.width = newLeftWidth + "%";
+  divider.style.left = newLeftWidth + "%";
+});
